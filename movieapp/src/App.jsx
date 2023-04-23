c91b4e7fd4291342fd98014299cfee9a8f182a0d
Hamza Nachawati <Hamza.nachawati77@gmail.com>
2023-04-23 19:06:43 +0200

    finish the header component with styles , fatch data from movie api

--- movieapp/src/App.jsx
+++ movieapp/src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Header/Navbar';
+import Home from './pages/Home';
 
 
 
 const App = () => {
   return <div className='App'>
     <Router>
-    <Navbar/>
+      <Navbar />
+      <Home/>
       <Routes>
-        <Route index element={<h1>Hello </h1>}></Route>
+        <Route index element={<Home />}></Route>
         <Route path='movie/:id' element={<h1>Movie details page</h1>} ></Route>
         <Route path='movie/:type' element={<h1>Movie list page</h1>} ></Route>
         <Route path='/*' element={<h1>Error page</h1>}></Route>
