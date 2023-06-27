cd4d98409986c274341cdc2de1cfbd60765e8a33
Hamza Nachawati <Hamza.nachawati77@gmail.com>
2023-06-27 12:51:11 +0200

    final

--- movieapp/src/pages/home/home.js
+++ movieapp/src/pages/home/home.js
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"
+import "./home.css"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import { Link } from "react-router-dom";
+import MovieList from "../../components/movieList/movieList";
 
-const Header = () => {
+const Home = () => {
+
    const [popularMovies, setPopularMovies] = useState([])
 
    useEffect(() => {
-      const getData = async () => {
-         const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
-         const data = await response.json()
-         setPopularMovies(data.results)
-
-         console.log(data.results)
-
-      }
-      getData()
+      fetch("https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US")
+         .then(res => res.json())
+         .then(data => setPopularMovies(data.results))
    }, [])
 
    return (
@@ -28,8 +26,8 @@ const Header = () => {
                showStatus={false}
             >
                {
-                  popularMovies.map((movie, id) => (
-                     <Link key={id} style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`} >
+                  popularMovies.map(movie => (
+                     <Link  to={`/movie/${movie.id}`} >
                         <div className="posterImage">
                            <img src={`https://image.tmdb.org/t/p/original${movie && movie.backdrop_path}`} />
                         </div>
@@ -48,11 +46,10 @@ const Header = () => {
                   ))
                }
             </Carousel>
-
-
+            <MovieList />
          </div>
       </>
    )
-};
+}
 
-export default Header;
+export default Home
